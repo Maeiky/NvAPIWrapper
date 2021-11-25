3840e5e2584744b295cbefbaee11d5f4e91dc871
Maeiky <maeiky@gmail.com>
2021-11-24 21:40:45 -0500

    GoldMiner

---
+++ GoldMiner/GoldMinerForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace GoldMiner {
+    partial class GoldMinerForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+        if (disposing && (components != null)) {
+        components.Dispose();
+        }
+        base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lsbGPU = new System.Windows.Forms.ListBox();
+            this.lbDriver = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lsbGPU
+            // 
+            this.lsbGPU.FormattingEnabled = true;
+            this.lsbGPU.Location = new System.Drawing.Point(118, 79);
+            this.lsbGPU.Name = "lsbGPU";
+            this.lsbGPU.Size = new System.Drawing.Size(531, 147);
+            this.lsbGPU.TabIndex = 0;
+            this.lsbGPU.SelectedIndexChanged += new System.EventHandler(this.lsbGPU_SelectedIndexChanged);
+            // 
+            // lbDriver
+            // 
+            this.lbDriver.AutoSize = true;
+            this.lbDriver.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDriver.Location = new System.Drawing.Point(96, 13);
+            this.lbDriver.Name = "lbDriver";
+            this.lbDriver.Size = new System.Drawing.Size(56, 18);
+            this.lbDriver.TabIndex = 1;
+            this.lbDriver.Text = "label1";
+            // 
+            // GoldMinerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbDriver);
+            this.Controls.Add(this.lsbGPU);
+            this.Name = "GoldMinerForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GoldMinerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbGPU;
+        private System.Windows.Forms.Label lbDriver;
+    }
+}
+
---
+++ GoldMiner/GoldMinerForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NvAPIWrapper;
+using NvAPIWrapper.Display;
+using NvAPIWrapper.GPU;
+using NvAPIWrapper.Mosaic;
+using NvAPIWrapper.Native;
+using NvAPIWrapper.Native.GPU.Structures;
+using NvAPIWrapper.Native.Interfaces.GPU;
+
+namespace GoldMiner {
+    public partial class GoldMinerForm:Form {
+        public GoldMinerForm() {
+        InitializeComponent();
+        }
+
+
+        private void PrintDriverVersion()
+        {
+              lbDriver.Text =  "NVIDIA Driver: "  + NVIDIA.DriverBranchVersion  + " [" + NVIDIA.DriverVersion  + "]: " +   NVIDIA.InterfaceVersionString;
+        }
+
+
+        private void GoldMinerForm_Load(object sender,EventArgs e) {
+           
+             PrintDriverVersion();
+
+
+         // find bits
+           
+
+
+
+
+
+
+
+             PhysicalGPU[] aGPU =  PhysicalGPU.GetPhysicalGPUs();
+
+            foreach (PhysicalGPU gpu in aGPU) { 
+                 string tVRAM = "";
+                 string tGPU = "";
+
+                ///////// Get Temp / VRAM /////////
+                var maxBit = 0;
+                for (; maxBit < 32; maxBit++){
+                    try { GPUApi.QueryThermalSensors(gpu.Handle, 1u << maxBit);}catch{break;}
+                }
+                if (maxBit != 0){
+                    var temp = GPUApi.QueryThermalSensors(gpu.Handle, (1u << maxBit) - 1);
+                    GPUThermalSensor[] GetThermalSettings = gpu.ThermalInformation.ThermalSensors.ToArray();
+                   string[]  QueryThermalSensors = temp.Temperatures.Take(maxBit).Select((t) => t.ToString("F2")).ToArray();
+                    tVRAM = QueryThermalSensors[QueryThermalSensors.Length-1];
+                    tGPU = QueryThermalSensors[0];
+                }
+                /////////////////////////////////
+             
+                GPUCooler[] aCoolers =  gpu.CoolerInformation.Coolers.ToArray();
+             
+                string fan = "Fans: ";
+                foreach (GPUCooler cooler in aCoolers) {
+                  // fan += cooler.CurrentFanSpeedInRPM + "% ";
+                   fan += cooler.CurrentLevel + "% ";
+                }
+                
+              //  gpu.BaseClockFrequencies
+               IClockFrequencies clk =  gpu.CurrentClockFrequencies;
+               ClockDomainInfo core_clk = clk.ProcessorClock;
+               ClockDomainInfo mem_clk = clk.MemoryClock;
+               ClockDomainInfo graph_clk = clk.GraphicsClock;
+               string _core_clk = core_clk.Frequency.ToString();
+               string _mem_clk = mem_clk.Frequency.ToString();
+               string _graph_clk = graph_clk.Frequency.ToString();
+
+              // string _core_clk =  gpu.clock;
+                
+
+               lsbGPU.Items.Add(  gpu.FullName  + " _core_clk[" + _graph_clk + "]"  + " _mem_clk[" + _mem_clk + "]"  +  " :" + " tGPU:" + tGPU + " tVRAM: " + tVRAM  + " " + gpu.BusInformation  + " : " +  " " + fan +  " " + gpu.ArchitectInformation);
+
+         //   gpu.GPUId
+               // lsbGPU.Items.Add(coolers.);
+            }
+        }
+
+        private void lsbGPU_SelectedIndexChanged(object sender,EventArgs e) {
+
+        }
+    }
+}
